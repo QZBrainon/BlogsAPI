ce05365214dfc47a8582a850be0b0a0fe54fd449
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-09-25 16:12:49 -0300

    Req02 OK

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',{
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },{
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  })
+  return User;
+}
+
+module.exports = UserSchema
